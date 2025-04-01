7a3a598b30962da439ae2b8ac37a43bb19156d01
manuvaldes15 <manuel.valdes@catolica.edu.sv>
2025-04-01 16:19:23 -0600

    first

---
+++ L02P02_2022MR655_2022VZ650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022MR655_2022VZ650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
